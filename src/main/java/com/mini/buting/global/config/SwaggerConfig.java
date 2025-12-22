6866681b47c58a5ee6ed484f79fd87f0fa17bb22
hyewon <dahxtq1@naver.com>
2025-12-22 23:32:38 +0900

    [BE] feat: 의존성 추가 및 Swagger API UI 설정 - SpringBoot 버전 설정(`3.5.5`) - 의존성 추가(`Lombok`, `Spring Web`, `Validation`, `Dotenv`, `Swagger`) - Swagger UI 설정

---
+++ src/main/java/com/mini/buting/global/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.mini.buting.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.mini.buting.global.constant.SecurityConstants.GRANT_TYPE;
+
+@Configuration
+public class SwaggerConfig {
+    @Value("${swagger.uri}")
+    private String uri;
+
+    @Bean
+    OpenAPI openAPI() {
+        String securityJwtName = "JWT";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+
+        Components components = new Components().addSecuritySchemes(securityJwtName,
+                new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(GRANT_TYPE.trim())
+                        .bearerFormat(securityJwtName)
+                        .description("헤더에 'Authorization: Bearer {ACCESS_TOKEN}' 형식으로 토큰을 입력해주세요."));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .addServersItem(new Server().url(uri))
+                .info(info());
+    }
+
+    private Info info() {
+        return new Info()
+                .title("Buting Spring API Docs")
+                .description("부울경 과팅을 위한 모바일 앱 서비스 '부팅' API 명세서")
+                .version("1.0.0");
+    }
+
+    @Bean
+    GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-api")
+                .pathsToMatch("/v1/**") // 이 경로에 해당하는 API 문서화
+                .build();
+    }
+}
