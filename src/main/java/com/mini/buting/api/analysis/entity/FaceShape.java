908534eac84370cb57a84d98cd21cbf043bfec1c
hyewon <dahxtq1@naver.com>
2026-01-03 00:24:27 +0900

    [BE] feat: 대학교 및 AI 분석 관련 도메인 엔티티 구현 - University, UniversityDomain, Major 엔티티 정의 - AI 분석 결과 매핑을 위한 FaceShape 엔티티 추가 - DomainCategory 등 관련 Enum 및 캐싱 로직 구현

---
+++ src/main/java/com/mini/buting/api/analysis/entity/FaceShape.java
@@ -0,0 +1,26 @@
+package com.mini.buting.api.analysis.entity;
+
+import com.mini.buting.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 외부에서 기본 생성자 호출 방지
+@AllArgsConstructor
+@Table(name = "face_shape", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_face_shape_name", columnNames = {"name"})
+})
+@Comment("AI 분석 얼굴형 카테고리 테이블")
+public class FaceShape extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("얼굴형 식별자")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    @Comment("얼굴형 명칭 (예: 강아지상, 고양이상)")
+    private String name;
+}
---
+++ src/main/java/com/mini/buting/api/university/entity/Major.java
@@ -0,0 +1,30 @@
+package com.mini.buting.api.university.entity;
+
+import com.mini.buting.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "major", indexes = {
+        @Index(name = "idx_major_title", columnList = "title")
+}, uniqueConstraints = {
+        @UniqueConstraint(name = "uk_major_title", columnNames = {"title"})
+})
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Comment("학과 정보 테이블")
+public class Major extends BaseTimeEntity {
+    @Id
+    @Comment("학과 고유 식별자")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    @Comment("학과 명칭 (예: 컴퓨터공학과)")
+    private String title;
+
+
+}
