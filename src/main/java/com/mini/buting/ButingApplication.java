982c66550c7feb9966c5485f1f26a3c48d481cb9
hyewon <dahxtq1@naver.com>
2026-01-03 00:22:20 +0900

    [BE] chore: JPA Auditing 활성화 및 JVM 타임존 설정 - @EnableJpaAuditing 추가를 통한 생성/수성 시간 자동화 - @PostConstruct를 활용하여 JVM 기본 타임존을 application.yaml 기반으로 설정 - 생성/수정 시간 자동 관리용 공통 엔티티 BaseTimeEntity 구현

--- src/main/java/com/mini/buting/ButingApplication.java
+++ src/main/java/com/mini/buting/ButingApplication.java
@@ -1,13 +1,27 @@
 package com.mini.buting;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class ButingApplication {
 
+    @Value("${spring.jpa.properties.hibernate.jdbc.time_zone}")
+    private String timeZone;
+
     public static void main(String[] args) {
         SpringApplication.run(ButingApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setJvmTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
     }
 
 }
