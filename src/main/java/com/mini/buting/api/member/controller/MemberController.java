1360b38180bb57353292e7a1c29d7cf4606d215b
정원준 <dnjswns3851@naver.com>
2026-01-07 00:11:15 +0900

    [BE] fix: member controller 엔드포인트 수정

--- src/main/java/com/mini/buting/api/member/controller/MemberController.java
+++ src/main/java/com/mini/buting/api/member/controller/MemberController.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/v1") // v1 버전 prefix 추가, /api 제거
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
@@ -19,44 +19,44 @@ public class MemberController {
      * 내 프로필 조회
      * TODO: Spring Security 적용 시 Authentication에서 사용자 정보 추출
      */
-    @GetMapping("/profile/me")
+    @GetMapping("/me/profile")  // /v1/me/profile
     public BaseResponse<MemberProfileResponse> getMyProfile(
-            @RequestHeader(value = "X-User-Id", required = false, defaultValue = "1") Long userId) {
-        
-        log.info("내 프로필 조회 요청: userId={}", userId);
-        
+                    @RequestHeader(value = "X-User-Id", required = false, defaultValue = "1") Long userId) {
+
+        log.debug("내 프로필 조회 요청: userId={}", userId);
+
         MemberProfileResponse profile = memberService.getMemberProfile(userId);
-        
-        log.info("내 프로필 조회 성공: userId={}, nickname={}", userId, profile.getNickname());
+
+        log.debug("내 프로필 조회 성공: userId={}, nickname={}", userId, profile.getNickname());
         return BaseResponse.onSuccess(profile);
     }
 
     /**
      * 특정 회원 프로필 조회
      */
-    @GetMapping("/profile/{memberId}")
-    public BaseResponse<MemberProfileResponse> getMemberProfile(@PathVariable Long memberId) {
-        
-        log.info("회원 프로필 조회 요청: memberId={}", memberId);
-        
-        MemberProfileResponse profile = memberService.getMemberProfile(memberId);
-        
-        log.info("회원 프로필 조회 성공: memberId={}, nickname={}", memberId, profile.getNickname());
+    @GetMapping("/users/{userId}/profile") // /v1/users/{userId}/profile
+    public BaseResponse<MemberProfileResponse> getMemberProfile(@PathVariable Long userId) {
+
+        log.debug("회원 프로필 조회 요청: userId={}", userId);
+
+        MemberProfileResponse profile = memberService.getMemberProfile(userId);
+
+        log.debug("회원 프로필 조회 성공: userId={}, nickname={}", userId, profile.getNickname());
         return BaseResponse.onSuccess(profile);
     }
 
     /**
-     * 닉네임으로 회원 프로필 조회
+     * 닉네임으로 회원 프로필 조회 (추후 구현 예정)
      */
-//    @GetMapping("/profile/search")
-//    public BaseResponse<MemberProfileResponse> getMemberProfileByNickname(
-//            @RequestParam String nickname) {
-//
-//        log.info("닉네임 검색으로 프로필 조회 요청: nickname={}", nickname);
-//
-//        MemberProfileResponse profile = memberService.getMemberProfileByNickname(nickname);
-//
-//        log.info("닉네임 검색 프로필 조회 성공: nickname={}", nickname);
-//        return BaseResponse.onSuccess(profile);
-//    }
+    //    @GetMapping("/users/search")
+    //    public BaseResponse<MemberProfileResponse> getMemberProfileByNickname(
+    //            @RequestParam String nickname) {
+    //
+    //        log.info("닉네임 검색으로 프로필 조회 요청: nickname={}", nickname);
+    //
+    //        MemberProfileResponse profile = memberService.getMemberProfileByNickname(nickname);
+    //
+    //        log.info("닉네임 검색 프로필 조회 성공: nickname={}", nickname);
+    //        return BaseResponse.onSuccess(profile);
+    //    }
 }
