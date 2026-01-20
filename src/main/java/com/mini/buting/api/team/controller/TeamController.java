211f89d666f5bf15cabf3ebeef54c40000acd088
정원준 <dnjswns3851@naver.com>
2026-01-20 16:52:06 +0900

    [BE] fix: API 엔드포인트 수정

--- src/main/java/com/mini/buting/api/team/controller/TeamController.java
+++ src/main/java/com/mini/buting/api/team/controller/TeamController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/teams")
+@RequestMapping("/v1/teams")
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Team", description = "팀 관련 API")
