bf8f69460a87055f165242d6f491005f085d33c4
hyewon <dahxtq1@naver.com>
2026-02-11 08:20:52 +0900

    [BE] feat: 회원가입 API Controller 뼈대 구현

--- src/main/java/com/mini/buting/api/member/controller/MemberController.java
+++ src/main/java/com/mini/buting/api/member/controller/MemberController.java
@@ -1,16 +1,22 @@
 package com.mini.buting.api.member.controller;
 
 import com.mini.buting.api.member.dto.MemberProfileResponse;
+import com.mini.buting.api.member.dto.SignUpRequestDto;
 import com.mini.buting.api.member.service.MemberService;
 import com.mini.buting.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/v1/members")
-@RequiredArgsConstructor
 @Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/members")
+@Tag(name = "Member", description = "사용자 관련 API")
 public class MemberController {
 
     private final MemberService memberService;
@@ -59,4 +65,14 @@ public class MemberController {
     //        log.info("닉네임 검색 프로필 조회 성공: nickname={}", nickname);
     //        return BaseResponse.onSuccess(profile);
     //    }
+
+    /**
+     * @implNote 소셜 로그인 이후 진행되는 API라, 소셜 정보 연계가 필요함.
+     */
+    @Operation(summary = "회원가입 API", description = "신규 사용자를 등록")
+    @PostMapping()
+    public BaseResponse<Void> signUp(@Valid @RequestBody SignUpRequestDto requestDto, HttpServletResponse response) {
+        // TODO: 회원가입 서비스 로직 호출
+        return BaseResponse.onSuccess();
+    }
 }
