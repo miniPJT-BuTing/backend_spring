a026fca50b1249e797a802bf3bc08d1710acadf8
hyewon <dahxtq1@naver.com>
2026-02-16 20:53:45 +0900

    [BE] chore: KakaoUserInfoFactory를 빈으로 등록

--- src/main/java/com/mini/buting/global/security/principal/oauth2/KakaoUserInfoFactory.java
+++ src/main/java/com/mini/buting/global/security/principal/oauth2/KakaoUserInfoFactory.java
@@ -2,10 +2,12 @@ package com.mini.buting.global.security.principal.oauth2;
 
 import com.mini.buting.api.member.domain.SocialProvider;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
 
 /**
  * <h2>Kakao 전용 사용자 정보 생성 팩토리</h2>
  */
+@Component
 public class KakaoUserInfoFactory implements OAuth2UserInfoFactory {
     @Override
     public boolean supports(SocialProvider providerName) {
